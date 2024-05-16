719fcc91d64e0cf599d4fe83049bc3bfa3b63efd
satyam rai <satyamrai@satyams-MacBook-Air.local>
2024-05-16 11:13:26 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import { Appbar } from "@/components/Appbar";
+import { NEXT_AUTH_CONFIG } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+async function getUser() {
+  const session = await getServerSession(NEXT_AUTH_CONFIG);
+  return session;
+}
+export default async function Home() {
+  const session = await getUser();
+  return (
+    <div>
+      <Appbar />
+      {JSON.stringify(session)}
+    </div>
+  );
+}
